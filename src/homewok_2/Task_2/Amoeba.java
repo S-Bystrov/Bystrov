c770e6603fea19ded0a48e3234ec90c1558c15dc
S-Bystrov <bystrov92@gmail.com>
2019-10-08 21:33:52 +0300

    initial comit

--- src/homewok_2/Task_2/Amoeba.java
+++ src/homewok_2/Task_2/Amoeba.java
@@ -1,4 +1,12 @@
 package homewok_2.Task_2;
 
 public class Amoeba {
+    public static void main(String[] args) {
+        int amoeb = 1;
+        int hour = 0;
+        for (; hour < 24; hour = hour + 3) {
+            amoeb = amoeb*2;
+            System.out.println(amoeb);
+        }
+    }
 }
