cfd55cc62db1babeec1026eb5ddbb7aed8118eab
S-Bystrov <bystrov92@gmail.com>
2019-10-08 21:05:07 +0300

    initial commit

---
+++ src/homewok_2/Task_1/Distance.java
@@ -0,0 +1,10 @@
+package homewok_2.Task_1;
+
+public class Distance {
+    public static void main(String[] args) {
+        double dist = 10.0;
+        for (int i=1; i < 7; i++){
+            dist = dist + dist * 0.1;}
+        System.out.println(dist);
+    }
+}
---
+++ src/homewok_2/Task_2/Amoeba.java
@@ -0,0 +1,4 @@
+package homewok_2.Task_2;
+
+public class Amoeba {
+}
