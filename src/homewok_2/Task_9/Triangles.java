ef0140f35c7f09784a040e17302d6e9c1f619917
S-Bystrov <bystrov92@gmail.com>
2019-10-11 01:06:32 +0300

    initial commit

---
+++ src/homewok_2/Task_9/Triangles.java
@@ -0,0 +1,50 @@
+package homewok_2.Task_9;
+
+public class Triangles {
+    public static void main(String[] args) {
+        int a=4;
+        for(int i=1; i<=a;i++){
+            for(int k=1; k<=a; k++){
+                if(k<=a-i)
+                    System.out.print(" ");
+                else
+                    System.out.print("*");
+            }
+            System.out.println();
+        }
+        System.out.println();
+
+        for(int b=1; b<=a;b++){
+            for(int c=1; c<=a; c++){
+                if(c>=b)
+                    System.out.print("*");
+                else
+                    System.out.print(" ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+
+        for(int d=1; d<=a;d++){
+            for(int e=1; e<=a; e++){
+                if(e<=d)
+                    System.out.print("*");
+                else
+                    System.out.print(" ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+
+        for(int f=1; f<=a;f++){
+            for(int g=1; g<=a; g++){
+                if(f<=a-g+1)
+                    System.out.print("*");
+                else
+                    System.out.print(" ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+}
