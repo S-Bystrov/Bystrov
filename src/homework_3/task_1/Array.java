3649d5b49d20428f057cf1b49f014c03d3d49abb
S-Bystrov <bystrov92@gmail.com>
2019-10-11 21:02:00 +0300

    initial commit

---
+++ src/homework_3/task_1/Array.java
@@ -0,0 +1,44 @@
+package homework_3.task_1;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Array {
+    public static void main(String[] args) {
+        int[] array = new int[3];
+        Scanner scOne = new Scanner(System.in);
+        System.out.println("Введите первый элемент массива: ");
+        int iOne = scOne.nextInt();
+
+        Scanner scTwo = new Scanner(System.in);
+        System.out.println("Введите второй элемент массива: ");
+        int iTwo = scTwo.nextInt();
+
+        Scanner scThree = new Scanner(System.in);
+        System.out.println("Введите третий элемент массива: ");
+        int iThree = scThree.nextInt();
+
+        array[0] = iOne;
+        array[1] = iTwo;
+        array[2] = iThree;
+        System.out.println(Arrays.toString(array));
+
+        int sum = 0;
+        for (int i = 0; i <array.length ; i++) {
+            sum = sum + array[i];
+        }
+        System.out.println("Сумма всех чисел равна: " + sum);
+
+        int max = 0;
+        int min = 0;
+        for (int i = 0; i <array.length ; i++) {
+            if(array[i]>array[max]){
+                max = i;
+            }
+            else if(array[i]<array[min]){
+                min = i;
+            }
+        }
+        System.out.println("Наибольшее число: " + array[max] + " Наименьшее число: " + array[min]);
+    }
+}
