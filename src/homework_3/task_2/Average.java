60018f3543e05dd914f3a094b44ac64cf4a87c6c
S-Bystrov <bystrov92@gmail.com>
2019-10-11 21:17:06 +0300

    initial commit

--- src/homework_3/task_2/Average.java
+++ src/homework_3/task_2/Average.java
@@ -3,7 +3,7 @@ package homework_3.task_2;
 public class Average {
     public static void main(String[] args) {
         int[] array = {4, 2, 3, 5, 6, 13};
-        double average;
+        int average;
         int sum = 0;
         for (int i = 0; i <array.length ; i++) {
             sum = sum + array[i];
