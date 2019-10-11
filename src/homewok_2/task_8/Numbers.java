b2d7f34caa5424f145003311e8f39589ea596d1f
Aleksandr Fomin <RezviyBelorus@gmail.com>
2019-10-11 20:08:56 +0300

    Update Numbers.java

--- src/homewok_2/task_8/Numbers.java
+++ src/homewok_2/task_8/Numbers.java
@@ -11,12 +11,17 @@ public class Numbers {
         int limit = 100;
         int sumEven=0;
         int sumOdd=0;
+        /*
+        эту задачу оптимально решить через 1 цикл и if-else
+        */
         for(int even=0; even<=limit; even++){
+            // even = четный и ты проверяешь, нелогично, стоит переименовать
             if(even%2 == 0){
                 sumEven += even;}
         }
         System.out.println("Сумма четных чисел от 0 до "+ limit +" равна " + sumEven);
         for(int odd=0; odd<=limit; odd++){
+         // odd = нечетный и ты проверяешь, нелогично, стоит переименовать
             if(odd%2 == 1){
                 sumOdd += odd;}
         }
