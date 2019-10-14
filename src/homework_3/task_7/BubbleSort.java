f042d22bd5a293ecb1f462f3d19fcd595eab20a1
S-Bystrov <bystrov92@gmail.com>
2019-10-15 02:28:36 +0300

    initial commit

---
+++ src/homework_3/task_7/BubbleSort.java
@@ -0,0 +1,33 @@
+package homework_3.task_7;
+
+import java.util.Arrays;
+
+public class BubbleSort {
+    public static void main(String[] args) {
+        int[] arrayBubble = new int[10];
+        for (int i = 0; i < arrayBubble.length; i++) {
+            arrayBubble[i] = (int) (Math.random() * 100);
+        }
+        System.out.println(Arrays.toString(arrayBubble));
+
+        boolean sort = true;
+
+        while (sort) {
+            sort = false;
+
+            for (int i = 1; i <arrayBubble.length ; i++) {
+                if(arrayBubble[i] < arrayBubble[i-1]){
+                    swap(arrayBubble, i, i-1);
+                    sort = true;
+                }
+            }
+        }
+        System.out.println(Arrays.toString(arrayBubble));
+    }
+    private static void swap(int[] array, int previous, int current){
+        int var = array[current];
+        array[current] = array[previous];
+        array[previous] = var;
+
+    }
+}
