15c0afd4f826662b6601f09803f33ae4b0228b76
Aleksandr Fomin <RezviyBelorus@gmail.com>
2019-10-16 20:55:27 +0300

    Update Inversion.java

--- src/homework_3/task_5/Inversion.java
+++ src/homework_3/task_5/Inversion.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 
 public class Inversion {
     public static void main(String[] args) {
-        int tempVar;
+        int tempVar; // создавай переменную максимально близко к месту, в котором будешь использовать
         int[] array = new int[10];
         for (int i = 0; i < array.length; i++) {
             array[i] = (int) (Math.random() * 100);
