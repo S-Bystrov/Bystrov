84a67c8b8c961749ca11c6216861a8c972a60896
Aleksandr Fomin <RezviyBelorus@gmail.com>
2019-10-16 20:51:18 +0300

    Update Array.java

--- src/homework_3/task_1/Array.java
+++ src/homework_3/task_1/Array.java
@@ -3,10 +3,12 @@ package homework_3.task_1;
 import java.util.Arrays;
 import java.util.Scanner;
 
+// форматируй код
 public class Array {
     public static void main(String[] args) {
         int[] array = new int[3];
         for (int i = 0; i < array.length; i++) {
+            // Scanner scanner = new Scanner(System.in); - один раз за пределами цикла, затем просто вызывай метод nextInt()
             Scanner scanner = new Scanner(System.in);
             System.out.println("Введите элемент массива:");
             int element = scanner.nextInt();
@@ -15,6 +17,7 @@ public class Array {
         System.out.println(Arrays.toString(array));
 
         int sum = 0;
+        // за 1 проход по массиву можно найти сумму, мин и макс. пока не оптимально
         for (int num: array) {
             sum += num;
         }
