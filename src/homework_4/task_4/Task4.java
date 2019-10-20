da8a20103c7073e895d9bb938c5f08e918fe6533
Aleksandr Fomin <RezviyBelorus@gmail.com>
2019-10-20 10:09:33 +0300

    Update Task4.java

--- src/homework_4/task_4/Task4.java
+++ src/homework_4/task_4/Task4.java
@@ -4,8 +4,11 @@ import homework_4.task_1.Human;
 
 public class Task4 {
     public static void main(String[] args) {
+         // human_1 -> sergey
         Human human_1 = new Human("Sergey", 27);
+        // human_2 -> noName
         Human human_2 = new Human();
+        // human_3 -> svetlana
         Human human_3 = new Human("Svetlana", 28);
         human_1.setText("Peoples");
         human_1.printText();
