54358799c274959adaa4abd768345e30334cf536
bystrov92@gmail.com <sergei7585609>
2019-10-22 18:39:45 +0200

    initial commit

--- src/homework_5/task_3/House.java
+++ src/homework_5/task_3/House.java
@@ -1,5 +1,13 @@
 package homework_5.task_3;
 
 public class House {
+    private int numOfFloor;
+    private int numOfTenants;
+    private boolean turnOnHeating;
+
+    public House (int numOfFloor, int numOfTenants) {
+        this.numOfFloor = numOfFloor;
+        this.numOfTenants = numOfTenants;
+    }
 
 }
---
+++ src/homework_5/task_3/HouseCatalog.java
@@ -0,0 +1,4 @@
+package homework_5.task_3;
+
+public class HouseCatalog {
+}
