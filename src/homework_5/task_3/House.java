12a71130e03ad3fbe1dbd3d9ff9970a7e63bf7a9
S-Bystrov <bystrov92@gmail.com>
2019-10-23 22:57:54 +0300

    initial commit

--- src/homework_5/task_3/House.java
+++ src/homework_5/task_3/House.java
@@ -1,13 +1,7 @@
 package homework_5.task_3;
 
-public class House {
-    private int numOfFloor;
-    private int numOfTenants;
-    private boolean turnOnHeating;
-
-    public House (int numOfFloor, int numOfTenants) {
-        this.numOfFloor = numOfFloor;
-        this.numOfTenants = numOfTenants;
-    }
-
+public interface House {
+    int getNumOfFloor();
+    int getNumOfTenants();
+    String getTurnOnHeating();
 }
