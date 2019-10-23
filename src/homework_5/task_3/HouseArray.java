c7b0500d3682b57dc7d4c3bfc92b721fb2fc4635
S-Bystrov <bystrov92@gmail.com>
2019-10-24 00:09:50 +0300

    initial commit

--- src/homework_5/task_3/HouseArray.java
+++ src/homework_5/task_3/HouseArray.java
@@ -1,9 +1,14 @@
 package homework_5.task_3;
 
+
+import java.util.Arrays;
+
 public class HouseArray {
     private final House[] house;
 
     public HouseArray(House[] house){
         this.house = house;
     }
+
+
 }
--- src/homework_5/task_3/HouseCatalog.java
+++ src/homework_5/task_3/HouseCatalog.java
@@ -2,6 +2,9 @@ package homework_5.task_3;
 
 public class HouseCatalog {
     public static void main(String[] args) {
+        CountryHouse countryHouse = new CountryHouse(1, 5, "heating is off");
+        CityHighRiseBuilding cityHighRiseBuilding = new CityHighRiseBuilding(4, 56, "heating turned on");
+        private final String[] houseCatalog = new String[2];
 
     }
 
