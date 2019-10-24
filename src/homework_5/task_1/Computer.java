4a3458392d02a10ad699dceeacad2ce176cf7186
Aleksandr Fomin <RezviyBelorus@gmail.com>
2019-10-25 00:52:26 +0300

    Update Computer.java

--- src/homework_5/task_1/Computer.java
+++ src/homework_5/task_1/Computer.java
@@ -1,5 +1,6 @@
 package homework_5.task_1;
 
+// я уже объяснил насчет дефолтных методов на занятии, старайся их не использовать
 public interface Computer {
     default void turnOn() {
         System.out.println("Turn on the computer");
