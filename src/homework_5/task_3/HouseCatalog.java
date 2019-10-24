e56efa685b4818e3d02e635b4429c764623cb2ed
S-Bystrov <bystrov92@gmail.com>
2019-10-24 19:58:21 +0300

    initial commit

--- src/homework_5/task_3/HouseCatalog.java
+++ src/homework_5/task_3/HouseCatalog.java
@@ -4,9 +4,11 @@ public class HouseCatalog {
     public static void main(String[] args) {
         CountryHouse countryHouse = new CountryHouse(1, 5, "heating is off");
         CityHighRiseBuilding cityHighRiseBuilding = new CityHighRiseBuilding(4, 56, "heating turned on");
-        private final House[] houseCatalog = new House[2];
-        //я пока додумываю способ задания массива
-
+        final House[] houseCatalog = new House[2];
+        houseCatalog[0] = cityHighRiseBuilding;
+        houseCatalog[1] = countryHouse;
+        houseCatalog[0].show();
+        houseCatalog[1].show();
     }
 
 }
