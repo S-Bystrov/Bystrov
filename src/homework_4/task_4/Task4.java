0c8878a9e541fe230c0a6dc75a9bfa606c50b143
S-Bystrov <bystrov92@gmail.com>
2019-10-24 20:37:05 +0300

    initial commit

--- src/homework_4/task_4/Task4.java
+++ src/homework_4/task_4/Task4.java
@@ -4,15 +4,15 @@ import homework_4.task_1.Human;
 
 public class Task4 {
     public static void main(String[] args) {
-         // human_1 -> sergey
-        Human human_1 = new Human("Sergey", 27);
-        // human_2 -> noName
-        Human human_2 = new Human();
-        // human_3 -> svetlana
-        Human human_3 = new Human("Svetlana", 28);
-        human_1.setText("Peoples");
-        human_1.printText();
-        human_2.printText();
-        human_3.printText();
+         // sergey -> sergey
+        Human sergey = new Human("Sergey", 27);
+        // noName -> noName
+        Human noName = new Human();
+        // svetlana -> svetlana
+        Human svetlana = new Human("Svetlana", 28);
+        sergey.setText("Peoples");
+        sergey.printText();
+        noName.printText();
+        svetlana.printText();
     }
 }
