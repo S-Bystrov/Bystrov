d7b0e37d8c11aebdd6acb7254c626715dcbea66f
Aleksandr Fomin <RezviyBelorus@gmail.com>
2019-10-25 01:02:32 +0300

    Update HouseCatalog.java

--- src/homework_5/task_3/HouseCatalog.java
+++ src/homework_5/task_3/HouseCatalog.java
@@ -4,6 +4,7 @@ public class HouseCatalog {
     public static void main(String[] args) {
         CountryHouse countryHouse = new CountryHouse(1, 5, "heating is off");
         CityHighRiseBuilding cityHighRiseBuilding = new CityHighRiseBuilding(4, 56, "heating turned on");
+        // House[] houseCatalog = {cityHighRiseBuilding, countryHouse}; - можно так
         final House[] houseCatalog = new House[]{cityHighRiseBuilding, countryHouse};
         for (House house : houseCatalog) {
             house.show();
