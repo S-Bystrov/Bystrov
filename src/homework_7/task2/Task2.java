634b9056dc5d3ce0c1f0d6072f778b02277dc743
S-Bystrov <bystrov92@gmail.com>
2019-10-25 20:29:21 +0300

    initial commit

---
+++ src/homework_7/task2/Task2.java
@@ -0,0 +1,10 @@
+package homework_7.task2;
+
+public class Task2 {
+    public static void main(String[] args) {
+        StringBuilder word = new StringBuilder("alucard");
+        System.out.println(word);
+        word = word.reverse();
+        System.out.println(word);
+    }
+}
