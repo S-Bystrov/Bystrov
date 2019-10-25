ffd7217b331503464d31a52c000e47d1447e737d
S-Bystrov <bystrov92@gmail.com>
2019-10-25 20:33:13 +0300

    initial commit

---
+++ src/homework_7/task3/Task3.java
@@ -0,0 +1,9 @@
+package homework_7.task3;
+
+public class Task3 {
+    public static void main(String[] args) {
+        String string = "Green. red. blue. yellow";
+        System.out.println(string);
+        System.out.println(string.replace(".", ","));
+    }
+}
