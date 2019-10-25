565e49b1f40c13674d6d23ec78e028f670c42873
S-Bystrov <bystrov92@gmail.com>
2019-10-25 20:24:10 +0300

    initial commit

---
+++ src/homework_7/task1/Task1.java
@@ -0,0 +1,9 @@
+package homework_7.task1;
+
+public class Task1 {
+    public static void main(String[] args) {
+        String word = "engineering";
+        System.out.println(word.substring(0, 6));
+        System.out.println(word.substring(7));
+    }
+}
