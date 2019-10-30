e470a2f614a043d2670ffd411a0085afa57702ba
Aleksandr Fomin <RezviyBelorus@gmail.com>
2019-10-30 22:38:11 +0300

    Update Task7.java

--- src/homework_7/task7/Task7.java
+++ src/homework_7/task7/Task7.java
@@ -6,27 +6,30 @@ public class Task7 {
         String text = " ";
         String addText = "hello";
 
+        // выноси такие куси кода в отдельные методы, которые будут возвращать время выполнения
         long start = System.currentTimeMillis();
         for (int i = 0; i < count; i++) {
             text = text.concat(addText);
         }
         long finish = System.currentTimeMillis();
-        System.out.println("Execution time" + (start - finish));
+        System.out.println("Execution time" + (start - finish)); // finish - start
 
+                // выноси такие куси кода в отдельные методы, которые будут возвращать время выполнения
         StringBuilder testStringBuilder = new StringBuilder("Test");
         start = System.currentTimeMillis();
         for (int i = 0; i < count; i++) {
             testStringBuilder.append(addText);
         }
         finish = System.currentTimeMillis();
-        System.out.println("Execution time StringBuilder " + (start - finish));
+        System.out.println("Execution time StringBuilder " + (start - finish));// finish - start
 
+                // выноси такие куси кода в отдельные методы, которые будут возвращать время выполнения
         StringBuffer testStringBuffer = new StringBuffer("Test");
         start = System.currentTimeMillis();
         for (int i = 0; i < count; i++) {
             testStringBuffer.append(addText);
         }
         finish = System.currentTimeMillis();
-        System.out.println("Execution time StringBuffer " + (start - finish));
+        System.out.println("Execution time StringBuffer " + (start - finish));// finish - start
     }
 }
