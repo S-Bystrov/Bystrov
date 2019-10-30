845f2bfb92d3787bb487f54d347b7201a7f18f94
S-Bystrov <bystrov92@gmail.com>
2019-10-30 10:06:38 +0300

    initial commit

--- src/homework_7/task8/Task8.java
+++ src/homework_7/task8/Task8.java
@@ -1,4 +1,22 @@
 package homework_7.task8;
 
+import java.util.Arrays;
+
 public class Task8 {
+    public static void main(String[] args) {
+        String[] carsType = {"1.4", "2.2", "1.6", "1.8"};
+        String firstTypeForChange = "1.4";
+        String secondTypeForChange = "1.8";
+        String replacement = "2.0";
+        String adding = "T";
+        for (int i = 0; i < carsType.length; i++) {
+            if (carsType[i] == firstTypeForChange) {
+                carsType[i] = carsType[i].replace(firstTypeForChange, replacement);
+            }
+            if (carsType[i] == secondTypeForChange) {
+                carsType[i] = carsType[i].concat(adding);
+            }
+        }
+        System.out.println(Arrays.toString(carsType));
+    }
 }
--- src/homework_8/task1/Task1.java
+++ src/homework_8/task1/Task1.java
@@ -3,15 +3,16 @@ package homework_8.task1;
 import java.util.Scanner;
 
 public class Task1 {
-    public static void main(String[] args) {
-        try {
-            Scanner number = new Scanner(System.in);
-            System.out.println("Enter two numbers: ");
-            int firstNum = number.nextInt();
-            int secondNUm = number.nextInt();
-            int result = firstNum % secondNUm
-            System.out.println(result);
-        } catch (N)
+   /* public static void main(String[] args) {
+       try{
+           Scanner number = new Scanner(System.in);
+           System.out.println("Enter two numbers: ");
+           int firstNum = number.nextInt();
+           int secondNUm = number.nextInt();
+           int result = firstNum%secondNUm
+           System.out.println(result);
+       }
+       catch (N)
 
-    }
+    }*/
 }
