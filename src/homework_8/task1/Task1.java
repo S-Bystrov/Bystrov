33c35af704812629c102d339e781ab0f30004f9a
S-Bystrov <bystrov92@gmail.com>
2019-10-30 10:12:36 +0300

    initial commit

--- src/homework_8/task1/Task1.java
+++ src/homework_8/task1/Task1.java
@@ -3,16 +3,17 @@ package homework_8.task1;
 import java.util.Scanner;
 
 public class Task1 {
-   /* public static void main(String[] args) {
+    public static void main(String[] args) {
        try{
            Scanner number = new Scanner(System.in);
            System.out.println("Enter two numbers: ");
            int firstNum = number.nextInt();
            int secondNUm = number.nextInt();
-           int result = firstNum%secondNUm
+           int result = firstNum % secondNUm;
            System.out.println(result);
+       } catch (ArithmeticException e) {
+           System.out.println("Division by zero is not allowed");
        }
-       catch (N)
 
-    }*/
+    }
 }
