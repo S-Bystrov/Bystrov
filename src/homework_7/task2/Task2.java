d3fa952549ca7ebc205c35b03aedea3bfb87f2b0
Aleksandr Fomin <RezviyBelorus@gmail.com>
2019-10-30 22:32:56 +0300

    Update Task2.java

--- src/homework_7/task2/Task2.java
+++ src/homework_7/task2/Task2.java
@@ -4,6 +4,7 @@ public class Task2 {
     public static void main(String[] args) {
         StringBuilder word = new StringBuilder("alucard");
         System.out.println(word);
+        // просто word.reverse(); Класс StringBuilder изменяемый, в отличие от String
         word = word.reverse();
         System.out.println(word);
     }
