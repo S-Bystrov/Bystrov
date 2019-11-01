3471cd9b2f1ad2cf842942632eca017de9f63705
S-Bystrov <bystrov92@gmail.com>
2019-11-01 21:36:15 +0300

    initial commit

---
+++ src/homework_9/task1/Task1.java
@@ -0,0 +1,26 @@
+package homework_9.task1;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+
+public class Task1 {
+    private static final String DIRECTORY = "src/homework_9/task1";
+
+    public static void main(String[] args) throws IOException {
+        File arrayFile = new File(DIRECTORY, "Array.txt");
+        String arrayFilePath = arrayFile.getAbsolutePath();
+        System.out.println(arrayFilePath);
+        try (FileOutputStream fos = new FileOutputStream(arrayFilePath);
+             ObjectOutputStream dos = new ObjectOutputStream(fos)) {
+            int size = 10;
+            int[] array = new int[size];
+            for (int i = 0; i < array.length; i++) {
+                array[i] = (int) (Math.random()) * 100;
+            }
+            dos.writeObject(array);
+            dos.flush();
+        }
+    }
+}
