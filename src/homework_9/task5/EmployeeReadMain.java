5318c1527327233faa0183e8f93d718778261f19
S-Bystrov <bystrov92@gmail.com>
2019-11-04 01:07:49 +0300

    initial commit

---
+++ src/homework_9/task5/EmployeeReadMain.java
@@ -0,0 +1,17 @@
+package homework_9.task5;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+
+public class EmployeeReadMain {
+    private static final String FILE_REFERENCE = "src/homework_9/task5/Employee";
+
+    public static void main(String[] args) throws IOException, ClassNotFoundException {
+        try (FileInputStream fis = new FileInputStream(FILE_REFERENCE);
+             ObjectInputStream ois = new ObjectInputStream(fis)) {
+            Employee employee = (Employee) ois.readObject();
+            System.out.println(employee);
+        }
+    }
+}
