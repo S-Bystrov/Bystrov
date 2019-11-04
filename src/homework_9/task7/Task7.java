c2478156298c88450cadbdf71a08cc20bfef90ce
S-Bystrov <bystrov92@gmail.com>
2019-11-04 12:39:58 +0300

    initial commit

--- src/homework_9/task7/Task7.java
+++ src/homework_9/task7/Task7.java
@@ -1,4 +1,20 @@
 package homework_9.task7;
 
+import java.io.File;
+
 public class Task7 {
+    private static final String FILE_REFERENCE = "src/";
+
+    public static void main(String[] args) {
+        File directory = new File(FILE_REFERENCE);
+        if (directory.isDirectory()) {
+            for (File item : directory.listFiles()) {
+                if (item.isDirectory()) {
+                    System.out.println(item.getName() + " = folder");
+                } else {
+                    System.out.println(item.getName() + " = file");
+                }
+            }
+        }
+    }
 }
