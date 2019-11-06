965ccb535cc65158e7af61a931e41aecdcc70ea8
S-Bystrov <bystrov92@gmail.com>
2019-11-06 20:50:31 +0300

    initial commit

---
+++ src/homework_10/task1/Airplanes.java
@@ -0,0 +1,39 @@
+package homework_10.task1;
+
+public class Airplanes<T, S> {
+    private T type;
+    private S maxSpeed;
+    private String countryOfProduction;
+
+    public <W> Airplanes(T type, S maxSpeed, W countryOfProduction) {
+        this.type = type;
+        this.maxSpeed = maxSpeed;
+        this.countryOfProduction = countryOfProduction.toString();
+    }
+
+    public T getType() {
+        return type;
+    }
+
+    public void setType(T type) {
+        this.type = type;
+    }
+
+    public S getMaxSpeed() {
+        return maxSpeed;
+    }
+
+    public void setMaxSpeed(S maxSpeed) {
+        this.maxSpeed = maxSpeed;
+    }
+
+    public String getCountryOfProduction() {
+        return countryOfProduction;
+    }
+
+    public void setCountryOfProduction(String countryOfProduction) {
+        this.countryOfProduction = countryOfProduction;
+    }
+
+
+}
---
+++ src/homework_10/task2/RenameStrategy.java
@@ -0,0 +1,4 @@
+package homework_10.task2;
+
+public enum RenameStrategy {
+}
