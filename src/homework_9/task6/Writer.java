0e82670ec9ac714096d9ce3ac630e7103319d441
Aleksandr Fomin <RezviyBelorus@gmail.com>
2019-11-10 15:20:17 +0300

    Update Writer.java

--- src/homework_9/task6/Writer.java
+++ src/homework_9/task6/Writer.java
@@ -8,6 +8,8 @@ public class Writer {
 
     public static void main(String[] args) throws IOException {
         try (FileWriter fr = new FileWriter(FILE_REFERENCE, true)) {
+            // когда записывал в файл, стоило запомнить с какого байта начинается твое сообщение, чтобы потом прочимтать его
+            // + кол-во байт в сообщение, чтобы знать сколько байт прочитать
             String text = "\n" + "Map of countries where Alexander Soloduha is listened to";
             fr.write(text);
             fr.flush();
