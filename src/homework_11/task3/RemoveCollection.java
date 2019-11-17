955cf6fcfff0c13eab6a5dcc3df19824e330080d
Aleksandr Fomin <RezviyBelorus@gmail.com>
2019-11-17 21:39:49 +0300

    Update RemoveCollection.java

--- src/homework_11/task3/RemoveCollection.java
+++ src/homework_11/task3/RemoveCollection.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+// +
 public class RemoveCollection {
     public static void main(String[] args) {
         List<Integer> firstCollection = new ArrayList<>();
