662d8f50963ecbe34c7345f957a28f96fac6c690
Aleksandr Fomin <RezviyBelorus@gmail.com>
2019-11-19 23:23:24 +0300

    Update Task1.java

--- src/homework_12/task1/Task1.java
+++ src/homework_12/task1/Task1.java
@@ -9,13 +9,14 @@ import java.util.List;
 public class Task1 {
     public static void main(String[] args) {
         int capacity = 20;
+        // стоило сделать класс с полем timeAdded и сетать его перед добавлением в хранилище
         List<String> warehouse = new ArrayList<>(capacity);
         productAdd(warehouse, "apple");
         productAdd(warehouse, "corn");
         System.out.println(warehouse);
     }
 
-    public static void productAdd(List warehouse, String product) {
+    public static void productAdd(List warehouse, String product) { // List<String>
         DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd/MM/yyy:hh-mm-ss.");
         warehouse.add(product + "| was added:" + LocalDateTime.now().format(dtf));
     }
