d022e6f22244c0e275cbff9c5508a90a30a86c7c
Aleksandr Fomin <RezviyBelorus@gmail.com>
2019-11-22 21:35:39 +0300

    Update Task4.java

--- src/homework13/task4/Task4.java
+++ src/homework13/task4/Task4.java
@@ -8,6 +8,7 @@ public class Task4 {
         Stream<Integer> integers = Stream.of(5, 4, 34, 28, 56, 22, 9);
         Optional<Integer> firstInteger = integers.filter(integer -> integer > 22)
                 .findFirst();
+      // а если в коллекции не будет числа > 22? будет исключение, когда ты вызовешь метод get(), добавь проверку
         System.out.println(firstInteger.get());
     }
 }
