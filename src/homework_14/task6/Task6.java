6bc407d9964304f8666f909cf879ccdaae1ba6a5
Aleksandr Fomin <RezviyBelorus@gmail.com>
2019-11-26 22:58:35 +0300

    Update Task6.java

--- src/homework_14/task6/Task6.java
+++ src/homework_14/task6/Task6.java
@@ -10,6 +10,7 @@ public class Task6 {
         int[] newArray = new int[arrayOne.length + arrayTwo.length];
         int i = arrayOne.length - 1, j = arrayTwo.length - 1, k = newArray.length;
         while (k > 0)
+            // строку ниже лучше разбить на что-то более понятное
             newArray[--k] = (j < 0 || (i >= 0 && arrayOne[i] >= arrayTwo[j])) ? arrayOne[i--] : arrayTwo[j--];
         System.out.println(Arrays.toString(newArray));
     }
