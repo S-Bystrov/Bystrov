ecd56e52944d810293223614eb240ad058dd4f30
Aleksandr Fomin <RezviyBelorus@gmail.com>
2019-12-03 20:30:40 +0300

    Update MyThread.java

--- src/homework_16/task1/MyThread.java
+++ src/homework_16/task1/MyThread.java
@@ -7,6 +7,7 @@ public class MyThread extends Thread {
 
     @Override
     public void run() {
+        // 11 - через конструктор
         for (int i = 0; i < 11; i++) {
             System.out.println(i);
         }
